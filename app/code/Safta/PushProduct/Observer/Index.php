764259fd73f87471fdb17aa8e0c95d6f6dcd6e82
Artem <jinnytrickz@gmail.com>
2018-09-13 12:22:18 +0300

    add flash message
    adding new product in cart, by url, redirect to cart and display  success message

--- app/code/Safta/PushProduct/Observer/Index.php
+++ app/code/Safta/PushProduct/Observer/Index.php
@@ -15,19 +15,21 @@ use Magento\Framework\Controller\ResultFactory;
 class Index implements ObserverInterface {
     protected $_responseFactory;
     protected $_url;
+    private $messageManager;
 
     public function __construct(
-
         \Magento\Framework\App\ResponseFactory $responseFactory,
-        \Magento\Framework\UrlInterface $url
+        \Magento\Framework\UrlInterface $url,
+        \Magento\Framework\Message\ManagerInterface $messageManager
     ) {
         $this->_responseFactory = $responseFactory;
         $this->_url = $url;
+        $this->messageManager = $messageManager;
     }
 
     public function execute(Observer $observer) {
         $event = $observer->getEvent();
-
+        $this->messageManager->addSuccess('Add new product in cart');
         $this->_responseFactory->create()->setRedirect('http://magento.loc/checkout/cart')->sendResponse();
         return $this;
     }
